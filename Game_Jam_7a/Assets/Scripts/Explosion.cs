50c558b06c3dec1646da589553453bffd1ee9330
pr0fessor-plum <30588650+pr0fessor-plum@users.noreply.github.com>
2019-08-09 13:42:48 -0400

    wand fireball

---
+++ Game_Jam_7a/Assets/Scripts/Explosion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        StartCoroutine(DestroyObject());
+    }
+
+    IEnumerator DestroyObject()
+    {
+        yield return new WaitForSeconds(1f);
+        Destroy(gameObject);
+    }
+}
---
+++ Game_Jam_7a/Assets/Scripts/FireballBehavior.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireballBehavior : MonoBehaviour
+{
+
+    public int strength;
+    public Vector3 direction;
+    [SerializeField]
+    private float _speed = 10;
+    [SerializeField]
+    private GameObject _explosion;
+
+    protected Rigidbody rb;
+
+    public void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Start()
+    {
+        StartCoroutine(ShootFireball());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Wand")
+        {
+            return;
+        }
+        else
+        {
+            Debug.Log("Collision at " + Time.time);
+            Instantiate(_explosion, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+        
+    }
+
+    IEnumerator ShootFireball()
+    {
+        yield return new WaitForSeconds(0.25f);
+        rb.velocity = transform.forward * _speed;
+
+    }
+    
+}
